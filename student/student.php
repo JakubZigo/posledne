37889c29a2ad3ccdf6b0f595ebc5ba5d3ec041a9
JakubZigo <xzigoj@stuba.sk>
2023-05-13 16:04:46 +0200

    sign out, user redirect, latex files

--- student/student.php
+++ student/student.php
@@ -14,29 +14,25 @@ require_once "../helpers/" . $_SESSION["lang"];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../base.css">
+    <link rel="stylesheet" href="../sign_in/sign_in.css">
 
     <title>Student</title>
 </head>
 <body>
 <div class="container">
     <div class="d-flex justify-content-between align-items-center">
-        <a href="../sign_in/sign_out.php"><i class="bi bi-door-closed" style="font-size: 2rem;"></i></a>
         <form method="post">
             <input type="hidden" name="lan" value="1">
             <button type="submit" id="lan"><i class="bi bi-translate m-lg-1" style="font-size: 2rem;"></i></button>
         </form>
+        <a href="../sign_in/sign_out.php"><i class="bi bi-door-closed" style="font-size: 2rem;"></i></a>
+
     </div>
 </div>
 
-<!--<div class="position-absolute top-0 start-50">-->
-<!--    <form method="post">-->
-<!--        <input type="hidden" name="lan" value="1">-->
-<!--        <button type="submit" id="lan"><i class="bi bi-translate m-lg-1" style="font-size: 2rem;"></i></button>-->
-<!--        <a class="rounded" href="../sign_in/sign_out.php"><i class="bi bi-door-closed" style="font-size: 4rem;"></i></a>-->
-<!--    </form>-->
-<!--</div>-->
-<div class="container center">
-    <h1 class="big-heading"><?php echo $lan['student']?></h1>
+<div class="container">
+    <div class="d-flex justify-content-between align-items-center">
+        <h1 class="big-heading"><?php echo $lan['student']?></h1>
+    </div>
 </div>
 </body>
